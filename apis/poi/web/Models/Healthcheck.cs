2042ab45887231e460fc999e91ff2e36618a20dc
Silva Sebastian Marcelo <sebas.lpb@gmail.com>
2021-09-14 15:01:47 -0300

    Initial commit

---
+++ apis/poi/web/Models/Healthcheck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace poi.Models
+{
+    public class Healthcheck
+    {
+        public Healthcheck()
+        {
+            Message = "POI Service Healthcheck";
+            Status = "Healthy";
+            //Status = "Broken...";
+        }
+        [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
+        public string Message {get;set;}
+
+        [Newtonsoft.Json.JsonProperty(PropertyName = "status")]
+        public string Status { get; set; }
+    }
+}
